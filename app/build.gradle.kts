5fe18773fef934b18dbe345f7e482cb2e1b9e113
Krispi3 <karinahaimov22@gmail.com>
2025-02-17 09:42:00 +0200

    jygj
    jgj

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.dagger:hilt-android:2.51.1")
 }
---
+++ app/src/main/java/com/example/mealmemoapp/utils/DataFuction.kt
@@ -0,0 +1,4 @@
+package com.example.mealmemoapp.utils
+
+///asychronized function to listen to live data and coroutines
+//fun<T,A> preformFetchingAndSaving
