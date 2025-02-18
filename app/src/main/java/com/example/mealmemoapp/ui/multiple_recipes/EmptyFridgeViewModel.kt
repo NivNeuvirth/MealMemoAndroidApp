0c8339a7247a7464aa591c5e14b5b22d7cfcbe43
Krispi3 <karinahaimov22@gmail.com>
2025-02-18 19:00:57 +0200

    mbn
    khbjmnb

---
+++ app/src/main/java/com/example/mealmemoapp/ui/multiple_recipes/EmptyFridgeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.mealmemoapp.ui.multiple_recipes
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class EmptyFridgeViewModel @Inject constructor(): ViewModel() {
+}
---
+++ app/src/main/java/com/example/mealmemoapp/ui/multiple_recipes/FavoritesFragment.kt
@@ -0,0 +1,9 @@
+package com.example.mealmemoapp.ui.multiple_recipes
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class FavoritesFragment:Fragment() {
+}
